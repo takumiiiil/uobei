c76c332290d36bb1c92be5e9c9be2e12c6df2dd9
takumiiiil <takuniwo@yandex.com>
2020-09-13 12:51:16 +0900

    Initial Commit

---
+++ sushiro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sushiro
+//
+//  Created by takumi on 2020/09/13.
+//  Copyright © 2020 takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
